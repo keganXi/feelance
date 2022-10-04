284e593cf1d5e7c04191156e5004dceb6050f5dd
Kegan Overberg <keganoverberg@Kegans-MacBook-Air.local>
2022-10-04 19:07:13 +0200

    added social links and icons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import React, {} from "react";
 import type { HeadFC } from "gatsby";
 
+
 import { FiChevronsDown } from "@react-icons/all-files/fi/FiChevronsDown";
+import { FiLinkedin } from "@react-icons/all-files/fi/FiLinkedin";
+import { FiTwitter } from "@react-icons/all-files/fi/FiTwitter";
+import { FiGithub } from "@react-icons/all-files/fi/FiGithub";
 
 
 // Components.
@@ -18,8 +22,16 @@ const IndexPage: React.FC = () => {
           <div className="m-auto space-y-1">
             <h1 className="text-[30px] sm:text-[60px]">Strategize Build Deploy</h1>
             <p className="sm:text-xl">Building amazing products that scale.</p>
-            <br></br>
-            <button className="bg-white hover:bg-neutral-200 rounded-lg text-primary px-8 py-3 text-xl hover:shadow-xl duration-200">Let's talk</button>
+            <br />
+
+            <div className="justify-center flex space-x-6">
+              <a href="#"><FiLinkedin size={20}/></a>
+              <a href="#"><FiTwitter size={20}/></a>
+              <a href="#"><FiGithub size={20}/></a>
+            </div>
+
+            <br />
+            <button className="bg-primary border-white border-2 rounded-lg text-white px-8 py-3 text-xl duration-200 hover:bg-white hover:text-primary">Let's talk</button>
           </div>
 
           <div className="absolute justify-center flex w-full bottom-20">
@@ -33,7 +45,6 @@ const IndexPage: React.FC = () => {
       </header>
 
       <main>
-        Hello World
 
       </main>
     </div>
