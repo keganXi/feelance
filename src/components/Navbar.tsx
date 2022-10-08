78294196a587086f4fd70fcdce40e928c4dcbd18
Kegan Overberg <keganoverberg@Kegans-MacBook-Air.local>
2022-10-08 16:13:29 +0200

    created menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 
 
+// Import icons.
+import { HiMenu } from "@react-icons/all-files/hi/HiMenu";
+import { AiOutlineClose } from "@react-icons/all-files/ai/AiOutlineClose";
+
+
 type LinkTypes = {
     title: string,
     link: string
@@ -11,11 +16,15 @@ interface NavbarPropTypes {
     readonly navOptions: {
         title: string,
         links: Array<LinkTypes>
-    }
+    },
+    general: {
+        menu: boolean
+    } 
 }
 
 
 const Navbar: React.FC = () => {
+    const [ menu, setMenu ] = useState<NavbarPropTypes["general"]["menu"]>(false);
     const [ navOption ] = useState<NavbarPropTypes["navOptions"]>({
         title: "Kegan Overberg",
         links: [
@@ -48,16 +57,42 @@ const Navbar: React.FC = () => {
     }
     
 
-    return(
-        <nav className="absolute z-20 top-0 w-full bg-transparent py-5 px-4 sm:px-24 flex text-white">
-            <div className="w-full">
-                <h4 className="text-base">{ navOption.title }</h4>
-            </div>
+    const openMenu = (): JSX.Element => {
+        return(
+            <div className="fixed z-40 bg-primary w-full h-screen text-center space-y-8 p-8">
+                <div className="right-5 absolute top-5">
+                    <button onClick={() => setMenu(false)}><AiOutlineClose size={25} color="white"/></button>
+                </div>
 
-            <div className="hidden sm:block w-full text-right space-x-12">
-                { navOption.links.map(item => navLinks(item)) }
+                {
+                navOption.links.map(
+                    item => <div><a className="text-white font-semibold text-2xl" href={ item.link }>{ item.title }</a></div>
+                    )}
             </div>
-        </nav>
+        );
+    }
+
+
+    return(
+        <>
+            <nav className="absolute z-20 top-0 w-full bg-transparent py-5 px-4 sm:px-24 flex text-white">
+                <div className="w-full">
+                    <h4 className="text-base">{ navOption.title }</h4>
+                </div>
+
+                <div className="hidden sm:block w-full text-right space-x-12">
+                    { navOption.links.map(item => navLinks(item)) }
+                </div>
+
+                <div className="sm:hidden">
+                    <button onClick={() => setMenu(true)}>
+                        <HiMenu size={27} />
+                    </button>
+                </div>
+            </nav>
+
+            { menu? openMenu() : null }
+        </>
     );
 }
 
